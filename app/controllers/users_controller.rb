08799e44f8827bf28f43a3325a754d0546f2666b
Maria <mliberov@gmail.com>
2015-02-26 19:21:10 -0500

    Add signup page #shipit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
@@ -18,5 +19,15 @@ class UsersController < ApplicationController
   end
 
   def show
+    # @user = User.find(params[:id])
   end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
 end
